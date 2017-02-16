4b8b28d608fd9312e3fd608cdcd307005968331c
Gerald Oliva <ryumaster001@yahoo.com>
2017-02-16 15:37:32 -0500

    Finished section 7

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,33 @@
-name1 = "larry"
-health1 = 60
+class Player
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
 
-puts "#{name1}'s health is #{health1 / 9.0}"
+  def to_s
+    "I'm #{@name} with a health of #{@health}."
+  end
 
-puts "\tlarry\n\tcurly\n\tmoe"
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+end
+
+player1 = Player.new("moe")
+puts player1
+
+player2 = Player.new("larry", 60)
+puts player2
+
+player3 = Player.new("curly", 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
