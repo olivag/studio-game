a5bdfd46fd35ffc29ead3b9d04e95c3e52885d58
Gerald Oliva <ryumaster001@yahoo.com>
2017-02-18 15:47:43 -0500

    Finished section 11

---
+++ player.rb
@@ -0,0 +1,45 @@
+class Player
+  attr_accessor :name
+  attr_reader :health, :score
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def score
+    @health + @name.length
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def strong?
+    @health > 100
+  end
+end
+
+if __FILE__ == $0
+  player = Player.new("moe")
+  puts player.name
+  puts player.health
+  player.w00t
+  puts player.health
+  player.blam
+  puts player.health
+end
